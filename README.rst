1436f537fe060d51e98ca3756b6a15aa43e95b46
Cameron Dawson <cdawson@mozilla.com>
2012-04-27 09:41:02 -0700

    added methods for getting runs, tests and environments.  the one for submitting them is not yet complete.  added a test case object to make it easier to set results on tests before submission

--- README.rst
+++ README.rst
@@ -7,11 +7,11 @@ Authentication
 
 - Login::
 
-    ```login(username, password)```
+    login(username, password)
 
 - Logout::
 
-    ```logout(username, password)```
+    logout(username, password)
 
 
 GET
@@ -19,31 +19,115 @@ GET
 
 - List of Test Runs::
 
-    ```get_runs(product, product_version)```
+    get_runs(product, product_version)
+
+    - **return**::
+
+        {
+            "meta": {
+                "limit": 20,
+                "next": null,
+                "offset": 0,
+                "previous": null,
+                "total_count": 3
+            },
+            "objects": [
+                {
+                    "description": "test run description",
+                    "id": "1",
+                    "name": "Alpha 1",
+                    "resource_uri": "/api/v1/run/1/"
+                },
+            ]
+        }
 
 - Test cases for specific test run::
 
-    ```get_cases(run_id, verbose)```
+    get_cases(run_id)
 
-  - **return**::
+    - **return**::
 
-      {
-          "run_id": run_id,
-          "cases: [
-            "id": id,
-            "summary": test_case_summary,
-            "status": status
-            ]
-          }
+        {
+
+            "cases": [
+                {
+                    "description": "A user can log in to the app.",
+                    "id": 1,
+                    "name": "Can log in.",
+                    "prefix_id": 1
+                },
+                {
+                    "description": "A new user can register for the app.\r\n**C:\\\\Users\\\\{your user name}\\\\**",
+                    "id": 2,
+                    "name": "Can register.",
+                    "prefix_id": "abc-xyz-2"
+                }
+            ],
+            "description": "test run description",
+            "id": "1",
+            "name": "Alpha 1",
+            "product_name": "MozTrap",
+            "productversion_version": "0.8",
+            "resource_uri": "/api/v1/runcases/1/"
+        }
+
+- Environments for specific test run::
+
+    get_environments(run_id)
+
+    - **return**::
+
+{
+
+    "description": "test run description",
+    "environments": [
+        {
+            "environment": [
+                "Chrome",
+                "Mandarin",
+                "Windows"
+            ],
+            "id": 1
+        },
+        {
+            "environment": [
+                "Chrome",
+                "Mandarin",
+                "OS X"
+            ],
+            "id": 2
+        },
+        {
+            "environment": [
+                "Chrome",
+                "Linux",
+                "Mandarin"
+            ],
+            "id": 3
+        },
+        {
+            "environment": [
+                "English",
+                "OS X",
+                "Safari"
+            ],
+            "id": 45
+        }
+    ],
+    "id": "1",
+    "name": "Alpha 1",
+    "resource_uri": "/api/v1/runenvironments/1/"
+
+}
 
 POST
 ----
 
 - Clone existing run::
 
-    ```clone_run(run_id)```
+    clone_run(run_id, new_name)
 
 - Submit test results::
 
-    ```submit_results(case_list)```
+    submit_results(case_list)
 
--- connect.py
+++ connect.py
@@ -1,6 +1,20 @@
 import requests
+from urllib import urlencode
+from json import loads
 
 class Connect:
+    """
+    Connect to MozTrap to fetch runs, cases and environments, and submit
+    test results for them
+
+    @@@ If there is a test that is different for two different environments,
+        then there will be separate test case IDs.  This is fine for manual
+        runs, but in the case of automation, it may need some other way to
+        determine the case it applies to.  Will the test script itself need
+        to keep track of which case id applies to which env?  Perhaps the
+        only option.
+
+    """
     def __init__(self, host, username, password, DEBUG=0):
         self.DEBUG = DEBUG
         self.host = host
@@ -8,50 +22,151 @@ class Connect:
         # ping the host to make sure it's valid?
 
 
-    """params are text name values of objects
-        Sample return JSON:
-        {"productId": 6,
-         "testCycleId": 14,
-         "testRunId": 3,
-         "tests": [
-             {"testCaseId": 10, "testResultId": 25, "testCaseName": "Special Test 1", "result": "pending"},
-             {"testCaseId": 11, "testResultId": 26, "testCaseName": "Special Test 2", "result": "pending"},
-             {"testCaseId": 12, "testResultId": 27, "testCaseName": "Special Test 3", "result": "pending"}
-         ]}
+    def get_runs(self, **kwargs):
+        """
+        Return a list of test runs.  This can be filtered by product or
+        productversion.
 
-    """
-    def get_tests(self, product, test_cycle, test_run):
-        '''Return a list of test result objects.  Pass/Fail can be applied to them'''
+        """
 
-        # may need to do individual calls to fetch the ids of the product, cycle and run?
+        params = {}
+        if "product" in kwargs:
+            params["productversion__product__name"] = kwargs.pop("product")
 
-        get_tests_url = "%s/included_test_results?product=%s&cycle=%s&run=%s" % ("host", "product", "test_cycle", "test_run")
+        if "productversion" in kwargs:
+            params["productversion__version"] = kwargs.pop("productversion")
 
-        r = requests.get(get_tests_url, auth=self.auth)
+
+        r = requests.get(
+            "http://{0}/api/v1/run?format=json&{1}".format(
+                self.host,
+                urlencode(params),
+                ))
+        return loads(r.text)
+
+
+    def get_environments(self, run_id):
+        """
+        Return a list of environments for the specified test run.
+
+        """
+
+        r = requests.get(
+            "http://{0}/api/v1/runenvironments/{1}?format=json".format(
+                self.host,
+                run_id,
+                ))
         assert r.status_code == 200
 
-        return r.content
+        return loads(r.text)["environments"]
 
-    """
-        Sample submission values:
-        {"productId": 6,
-         "testCycleId": 14,
-         "testRunId": 3,
-         "tests": [
-             {"testCaseId": 10, "testResultId": 25, "testCaseName": "Special Test 1", "result": "pending"},
-             {"testCaseId": 11, "testResultId": 26, "testCaseName": "Special Test 2", "result": "pending"},
-             {"testCaseId": 12, "testResultId": 27, "testCaseName": "Special Test 3", "result": "pending"}
-         ],
-         "environment": {"category1": "value1", "category2": "value2", "category3": "value3"}
-         }
+
+    def get_environment_id(self, run_id, element_list):
         """
-    def submit_results(self, tests, environment):
-        '''Submit the tests back to the system with results.  Results are not required for any of the tests.  '''
-        test_result_list
-        set_results_url = "%s/submit_results" % (self.host)
+        Return a single environment id for the specified test run that
+        matches the element_list of strings.
+
+        @@@ This is probably more efficiently handled on the server in a
+        different endpoint
+        """
+
+        envs = self.get_environments(run_id)
+        exp_env = set(element_list)
+
+        try:
+            env_id = next(item["id"] for item in envs if
+                exp_env == set(item["environment"]))
+
+        except:
+            raise EnvironmentDoesNotExistException(
+                "Run with id {0} does not have environment: {1}".format(
+                    run_id,
+                    element_list,
+                    ))
+
+        return env_id
+
+    def get_tests(self, run_id):
+        """
+        Return a list of TestCase objects.  Pass/Fail/Invalid is set on each
+        object in the list.
+
+        """
+
+        r = requests.get("http://{0}/api/v1/runcases/{1}?format=json".format(
+            self.host,
+            run_id,
+            ))
+        assert r.status_code == 200
+
+        return [TestCase(x) for x in loads(r.text)["cases"]]
+
+
+    def post_results(self, test_cases, environment, auth):
+        """
+        Submit the tests back to the system with results.
+        Results are not required for any of the tests.
+
+        """
+
+        data = {"cases": test_cases,
+                "environment": environment,
+                }
 
         if DEBUG:
-            print "host=%s" % self.host
+            print "host={0}".format(self.host)
 
-        r = requests.post(get_tests_url, data=tests, auth=self.auth)
+        r = requests.post(
+            "http://{0}/api/v1/runresults/{1}?format=json".format(
+                self.host,
+                run_id,
+                ),
+            data=loads(data), auth=auth,
+        )
         assert r.status_code == 200
+
+
+
+class TestCase(object):
+    FAIL = "fail"
+    PASS = "pass"
+    INVALID = "invalid"
+    PENDING = "pending"
+
+    def __init__(self, data):
+        self.name = data["name"]
+        self.id = data["id"]
+        self.prefix_id = data["prefix_id"]
+        self.description = data["description"]
+        self.state = {"result": TestCase.PENDING}
+
+
+    def __str__(self):
+        return "<TestCase - name: {0}, id: {1}, prefix_id: {2}, description: {3}, state: {4}".format(
+            self.name,
+            str(self.id),
+            str(self.prefix_id),
+            self.description,
+            str(self.state),
+        )
+
+
+    def markpass(self):
+        self.state = {"result": TestCase.PASS}
+
+
+    def markfail(self, comment, bug_url=None):
+        self.state = {
+            "result": TestCase.FAIL,
+            "comment": comment,
+            "bug_url": bug_url,
+            }
+
+    def markinvalid(self, comment):
+        self.state = {
+            "result": TestCase.INVALID,
+            "comment": comment,
+            }
+
+class EnvironmentDoesNotExistException(Exception):
+    pass
